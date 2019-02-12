7f022e66d0797593b17880813e8cc8e21152af95
Oliver <ofcharrison@gmail.com>
2019-02-12 17:02:45 +0000

    Test for incomplete journey

--- spec/journeyLog_spec.rb
+++ spec/journeyLog_spec.rb
@@ -17,4 +17,10 @@ describe JourneyLog do
     expect(journeyLog.current_journey).to have_attributes(:entrystation => nil, :exitstation => nil)
     # a kind of Journey with nil entry and exit stations
   end
+
+  it "returns incomplete journey if in progress" do
+  journeyLog = JourneyLog.new(Journey)
+  journeyLog.start("Paddington")
+  expect(journeyLog.current_journey).to have_attributes(:entrystation => "Paddington", :exitstation => nil)
+  end
 end
