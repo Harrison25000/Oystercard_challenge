85783a60de8f78aabca3e75b11ebc527356a3f33
Oliver <ofcharrison@gmail.com>
2019-02-12 16:42:35 +0000

    write method start

--- spec/journeyLog_spec.rb
+++ spec/journeyLog_spec.rb
@@ -2,7 +2,14 @@ require 'journeyLog'
 
 describe JourneyLog do
   it "it stores a journey class" do
-    journeyLog = JourneyLog(Journey)
+    journeyLog = JourneyLog.new(Journey)
     expect(journeyLog.journey_class).to eq(Journey)
+  end
+
+  it "Starts a journey with an entry station" do
+    journeyLog = JourneyLog.new(Journey)
+    journeyLog.start("Paddington")
+    expect(journeyLog.journey).to include(a_kind_of(Journey))
+
   end
 end
