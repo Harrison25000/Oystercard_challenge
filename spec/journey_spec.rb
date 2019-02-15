fd5677b259d79e9d91b626591b444cfd9008b625
James Ault <james.ault@johnlewis.co.uk>
2019-02-15 16:48:20 +0000

    Step 16 Tests for varied fare created

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -34,4 +34,35 @@ describe Journey do
    expect(journey.fare).to eq 1
  end
 
+ context 'Returns a fare of' do
+
+  let (:station_double) {double :station}
+  let (:station_double2) {double :station}
+
+  before :each do
+    @journey = Journey.new(station_double)
+    @journey.end(station_double2)
+  end
+
+  it '2 when going from zone 1 to 2' do
+    zones(1,2)
+    expect(@journey.fare).to eq 2
+ end
+
+ it '5 when going from zone 5 to 1' do
+  zones(5,1)
+  expect(@journey.fare).to eq 5
+end
+
+it '1 when going from zone 1 to 1' do
+  zones(1,1)
+  expect(@journey.fare).to eq 1
+end
+end
+end
+
+
+def zones(n,m)
+  allow(station_double).to receive(:zone).and_return(n)
+  allow(station_double2).to receive(:zone).and_return(m)
 end
